3eb33f40ccb5e7a613fe57b58ce546a25c5ce516
Susheel Arukonda <asusheel23b@gmail.com>
2021-01-23 23:56:41 +0530

    Class To be added

---
+++ Practice-3/4.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+void subsetsum(vector<int>v,int n, int k)
+{
+	ll sz=1<<(v.size()-1);
+
+	for(int i=1;i<=sz;i++)
+	{
+		ll sum=0;
+		for(int j=0;j<v.size();j++)
+		{
+			if(i&(1<<j))
+			{
+				sum=sum+v[j];
+			}
+		}
+		if(sum==k){
+		    for(int j=0;j<v.size();j++)
+			   {
+				  if(i&(1<<j))
+				  {
+					 cout<<v[j]<<" ";
+				  }
+			   }
+			   cout<<endl;
+		}
+	}
+	cout<<"No"<<endl;
+}
+
+
+int main()
+{
+	int n;
+	cin>>n;
+	vector<int>v(n);
+
+	for(int i=0;i<n;i++)
+		cin>>v[i];
+
+	int k;
+	cin>>k;
+
+	subsetsum(v,n,k);
+
+}

fd0523966ee5097712ce649652a68fc553835e01
Susheel Arukonda <asusheel23b@gmail.com>
2021-02-21 23:44:13 +0530

    classes not added

--- Practice-4/1.cpp
+++ Practice-4/1.cpp
@@ -26,6 +26,99 @@ int way2(int *arr,int s,int e)
 	return max(a,b);
 }
 
+int findmax_3way(int arr[],int low, int high)
+{
+
+    if (low == high){
+        return arr[low];
+    }
+
+    if(low>high){
+        return -9999;
+    }
+
+    int mid = (high - low + 1)/3;
+    if(mid == 0){
+        mid = 1;
+    }
+    int mid1 = low + mid - 1;
+    int mid2 = mid1 + mid;
+
+    int m1 = findmax_3way(arr,low, mid1);
+    int m2 = findmax_3way(arr,mid1 + 1, mid2);
+    int m3 = findmax_3way(arr,mid2 + 1, high);
+
+    if(m1>m2){
+        if(m1>m3){
+            return m1;
+        }
+        else{
+            return m3;
+        }
+    }
+    else{
+        if(m2>m3){
+           return m2;
+        }
+        else{
+            return m3;
+        }
+    }
+}
+
+int findmax_kway(int arr[],int low, int high, int k)
+{
+
+    if (low == high){
+      return arr[low];
+    }
+
+    if(low>high){
+        return -9999;
+    }
+
+    int mids[k];
+    int mid = (high - low + 1)/k;
+    if(mid == 0){
+        mid = 1;
+    }
+    mids[0] = low + mid - 1;
+    for(int i = 1; i<k; i++){
+        if(i>(high - low)){
+            mids[i] = mids[0];
+        }
+        else{
+            mids[i] = mids[i-1] + mid;
+        }
+
+    }
+
+    int m1 = findmax_kway(arr,low, mids[0], k);
+    int maxi = m1;
+
+    for(int i=0; i<k-1; i++){
+
+        int m2 = findmax_kway(arr,mids[i] + 1, mids[i+1], k);
+        if(maxi<m2){
+            maxi = m2;
+        }
+    }
+
+
+    int m3 = findmax_kway(arr,mids[k-2] + 1, high, k);
+
+    if(m3>maxi){
+
+        // cout<<m3<<"\n";
+        return m3;
+    }
+    else{
+        // cout<<maxi<<"\n";
+        return maxi;
+    }
+}
+
+
 int main()
 {
 	int n;
@@ -37,4 +130,6 @@ int main()
 	}
 	cout<<"1-Way "<<way1(arr,n)<<endl;
 	cout<<way2(arr,0,n-1)<<endl;
+	cout<<"3-way "<<findmax_3way(arr,0,n-1)<<endl;
+	cout<<"k-way "<<findmax_kway(arr,0,n-1,4)<<endl;
 }
--- Practice-4/2.cpp
+++ Practice-4/2.cpp
@@ -14,6 +14,32 @@ int way2(int *arr,int s,int e)
 	return way2(arr,s,mid)+way2(arr,mid+1,e);
 }
 
+int findmax_3way(int arr[],int low, int high)
+{
+
+    if (low == high){
+        if(arr[low]<0)
+            return 1;
+        else 
+            return 0;
+    }
+
+
+    int mid = (high - low + 1)/3;
+    if(mid == 0){
+        mid = 1;
+    }
+    int mid1 = low + mid - 1;
+    int mid2 = mid1 + mid;
+
+    int m1 = findmax_3way(arr,low, mid1);
+    int m2 = findmax_3way(arr,mid1 + 1, mid2);
+    int m3 = findmax_3way(arr,mid2 + 1, high);
+
+    return m1+m2+m3;
+
+}
+
 int main()
 {
 	int n;
@@ -24,5 +50,6 @@ int main()
 		cin>>arr[i];
 	}
 	cout<<way2(arr,0,n-1)<<endl;
+	cout<<findmax_3way(arr,0,n-1)<<endl;
 
 }

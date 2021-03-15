6b03092fd55b6f7e3cb612f91748c8d701543ae7
Susheel Arukonda <asusheel23b@gmail.com>
2021-03-15 10:44:26 +0530

    modified bubble sort function

--- Practice-5/1.cpp
+++ Practice-5/1.cpp
@@ -1,17 +1,9 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main()
+void bubbleSort(vector<int>v)
 {
-	int n,temp;
-	cout<<"Enter the no.of elements"<<endl;
-	cin>>n;
-	vector<int>v(n);
-	cout<<"Enter the elements"<<endl;
-	for(int i=0;i<n;i++)
-	{
-		cin>>v[i];
-	}
+	int temp,n=v.size();
 	for(int i=0;i<n;i++)
 	{
 		for(int j=0;j<n-i-1;j++)
@@ -24,10 +16,26 @@ int main()
 			}
 		}
 	}
+	cout<<endl;
 	cout<<"Bubble Sort-> ";
 	for(int i=0;i<n;i++)
 	{
 		cout<<v[i]<<" ";
 	}
 	cout<<endl;
+
+}
+
+int main()
+{
+	int n;
+	cout<<"Enter the no.of elements"<<endl;
+	cin>>n;
+	vector<int>v(n);
+	cout<<"Enter the elements"<<endl;
+	for(int i=0;i<n;i++)
+	{
+		cin>>v[i];
+	}
+	bubbleSort(v);
 }

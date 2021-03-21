a2bfcaed56da16e6623d1e4eeeb1e54e48fc5d46
Susheel Arukonda <asusheel23b@gmail.com>
2021-03-21 13:01:42 +0530

    added inheritance

--- Practice-5/1.cpp
+++ Practice-5/1.cpp
@@ -1,7 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void print(int v[],int n)
+class Parent{
+public:
+	void bubbleSort(int *v,int s,int e);
+	void selectionSort(int *v,int s,int e);
+	void insertionSort(int *v,int s,int e);
+	void print(int v[],int n);
+
+
+};
+
+class Child:public Parent{
+public:
+	void merge(int a[],int s,int mid,int e);
+	void mergesort_bubble(int a[],int s,int e);
+	void mergesort_selection(int a[],int s,int e);
+	void mergesort_insertion(int a[],int s,int e);
+
+};
+
+void Parent::print(int v[],int n)
 {
 	for(int i=0;i<n;i++)
 	{
@@ -10,7 +29,7 @@ void print(int v[],int n)
 	cout<<endl;
 }
 
-void bubbleSort(int *v,int s,int e)
+void Parent::bubbleSort(int *v,int s,int e)
 {
 	int temp;
 	for(int i=s;i<=e;i++)
@@ -26,23 +45,15 @@ void bubbleSort(int *v,int s,int e)
 		}
 	}
 	cout<<endl;
-	cout<<"Bubble Sort-> ";
-	//print(v);
-	for(int i=s;i<=e;i++)
-	{
-		cout<<v[i]<<" ";
-	}
-	cout<<endl;
-
 }
 
-void insertionSort(vector<int>v)
+void Parent::insertionSort(int *v,int s,int e)
 {
-	int n=v.size(),j=0,temp;
-	for(int i=1;i<n;i++)
+	int j=0,temp;
+	for(int i=s+1;i<=e;i++)
 	{
 		j=i;
-		while(j!=0 && v[j]<v[j-1])
+		while(j!=s && v[j]<v[j-1])
 		{
 			temp=v[j];
 			v[j]=v[j-1];
@@ -50,17 +61,15 @@ void insertionSort(vector<int>v)
 			j--;
 		}
 	}
-	cout<<"Insertion Sort-> ";
-	//print(v);
 }
 
-void selectionSort(vector<int>v)
+void Parent::selectionSort(int *v,int s,int e)
 {
-	int p,i,j,min1,ind,n=v.size();
-	for( i=0;i<n;i++)
+	int p,i,j,min1,ind;
+	for( i=s;i<=e;i++)
 	{
 		min1=v[i];p=0;
-		for( j=i+1;j<n;j++)
+		for( j=i+1;j<=e;j++)
 		{
 			if(min1>v[j])
 			{
@@ -74,11 +83,10 @@ void selectionSort(vector<int>v)
 		v[i]=min1;
 	}
 	}
-	cout<<"selection Sort-> "<<endl;
-	//print(v);
+
 }
 
-void merge(int a[],int s,int mid,int e)
+void Child::merge(int a[],int s,int mid,int e)
 {
 	int i=s,j=mid+1,k=s;
 	int b[e+1];
@@ -108,48 +116,89 @@ void merge(int a[],int s,int mid,int e)
 	}
 }
 
-void mergesort(int a[],int s,int e)
+void Child::mergesort_bubble(int a[],int s,int e)
 {
 	int mid=s+(e-s)/2;
 	if(e-s<5 and s<e)
 	{
-		cout<<"YES"<<endl;
 		bubbleSort(a,s,e);
 		return ;
 	}
 	else if(s<e)
 	{
-		cout<<"No"<<endl;
-		// int mid=s+(e-s)/2;
-
-		mergesort(a,s,mid);
-		mergesort(a,mid+1,e);
+		mergesort_bubble(a,s,mid);
+		mergesort_bubble(a,mid+1,e);
 	}
 
 	merge(a,s,mid,e);
 	
 }
 
+void Child::mergesort_selection(int a[],int s,int e)
+{
+	int mid=s+(e-s)/2;
+	if(e-s<5 and s<e)
+	{
+		selectionSort(a,s,e);
+		return ;
+	}
+	else if(s<e)
+	{
+		mergesort_selection(a,s,mid);
+		mergesort_selection(a,mid+1,e);
+	}
+
+	merge(a,s,mid,e);
+	
+}
+
+void Child::mergesort_insertion(int a[],int s,int e)
+{
+	int mid=s+(e-s)/2;
+	if(e-s<5 and s<e)
+	{
+		insertionSort(a,s,e);
+		return ;
+	}
+	else if(s<e)
+	{
+		mergesort_insertion(a,s,mid);
+		mergesort_insertion(a,mid+1,e);
+	}
+
+	merge(a,s,mid,e);
+	
+}
+
+
+
 int main()
 {
 	int n;
 	cout<<"Enter the no.of elements"<<endl;
 	cin>>n;
-	vector<int>v(n);
+
 	int a[n];
 	cout<<"Enter the elements"<<endl;
 	for(int i=0;i<n;i++)
 	{
 		cin>>a[i];
 	}
-	//bubbleSort(a,0,4);
-	//insertionSort(v);
-	//selectionSort(v);
-	mergesort(a,0,n-1);
-	// for(int i=0;i<n;i++)
-	// {
-	// 	cout<<a[i]<<" ";
-	// }
-	// cout<<endl;
-	print(a,n);
+
+
+	Child ob;
+
+	cout<<"--------merge+bubble--------"<<endl;
+	ob.mergesort_bubble(a,0,n-1);
+	ob.print(a,n);
+
+
+	cout<<"--------merge+selection--------"<<endl;
+	ob.mergesort_selection(a,0,n-1);
+	ob.print(a,n);
+
+
+	cout<<"--------merge+insertion--------"<<endl;
+	ob.mergesort_insertion(a,0,n-1);
+	ob.print(a,n);
 }

8e4e03c8befb35920ea0df1919e4e7a9b3d77382
Susheel Arukonda <asusheel23b@gmail.com>
2021-04-11 13:46:27 +0530

    min sum array

---
+++ Practice-6/4.cpp
@@ -0,0 +1,37 @@
+//Minimum sum of two numbers formed from digits of an array
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int solve(vector<int>v)
+{
+	sort(v.begin(), v.end());
+
+	int a = 0, b = 0;
+	for (int i = 0; i < n; i++)
+	{
+		if (i & 1)
+			a = a*10 + v[i];
+		else
+			b = b*10 + v[i];
+	}
+
+
+	return a + b;
+}
+
+int main()
+{
+	int n;
+	cout<<"Enter the no.of elements"<<endl;
+	cin>>n;
+
+	vector<int>a(n);
+
+	cout<<"Enter the elements"<<endl;
+	for(int i=0;i<n;i++)
+	{
+		cin>>a[i];
+	}
+	cout << solve(a) << endl;}
